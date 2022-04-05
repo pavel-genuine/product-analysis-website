a7cc8457ae4507d0fcc4e7065aea7b9947516861
pavel.genuine <pavel.play@gmail.com>
2022-04-05 15:03:01 +0600

    finalized

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -8,11 +8,14 @@ const Blogs = () => {
                 createContext , useContext hooks are used for this feature. createContext returns provider and consumer.
             </p>
             <h2>Semantic Tags</h2>
-            <p>Semantic HTML elements are those that clearly describe their meaning in a human- and machine-readable way.
+            <p>Semantic HTML elements are those that clearly describe their meaning in a human- and machine-readable way. The semantic HTML tags help the search engines and other user devices to determine the importance and context of web pages.
+The pages made with semantic elements are much easier to read.
+It has greater accessibility. It offers a better user experience.
 
             Elements such as header, footer and article are all considered semantic because they accurately describe the purpose of the element and the type of content that is inside them.</p>
             <h2>Inline , Block and Inline-block elements </h2>
-            <p>Which elements placed in a same line and could not change height wise style properties those are Inline elements, Block elemnts placed in different block and could modify both height and width , and Inline-block elements placed in a samne line but behave like block element</p>
+            <p> They consume the entire width available irrespective of their sufficiency. They always start in a new line and have top and bottom margins. It does not contain any other elements next to it. Inline elements occupy only enough width that is sufficient to it and allows other elements next to it which are inline. Inline elements don’t start from a new line and don’t have top and bottom margins as block elements have.
+                Which elements placed in a same line and could not change height wise style properties those are Inline elements, Block elemnts placed in different block and could modify both height and width , and Inline-block elements placed in a samne line but could behave like block element</p>
 
 
         </div>
